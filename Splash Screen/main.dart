b41da433aceeca5f231fba67f8cf9154f15de4ee
Mohan SPM <40892986+cloudstarmohan@users.noreply.github.com>
2020-07-08 08:38:26 +0530

    splash screen

---
+++ Splash Screen/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen()
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState(){
+    super.initState();
+
+    _mockCheckForSession().then(
+       _navigateToHome();
+    );
+  }
+
+  Future<bool> _mockCheckForSession() async {
+    await Future.delayed(Duration(milliseconds: 6000), () {});
+    return true;
+  }
+
+  void _navigateToHome(){
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (BuildContext context) => HomeScreen()
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+          Text('Welcome To Cloudstar'),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar (title: Text('Home')),
+        body: Center(
+          child: Card(
+            color: Colors.grey[300],
+            elevation: 10.0,
+            child: Container(
+                height: 100.0,
+                width: 100.0,
+                alignment: Alignment.center,
+                child: Text(
+                  'Welcome To CloudstarMohan',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0
+                  ),
+                )
+            ),
+          ),
+        )
+    );
+  }
+}
